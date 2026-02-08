181c04da0ff1e14d285019fa06c3b2f67ea46169
Palgun Shashi Kumar <skchinna7@gmail.com>
2026-02-08 01:04:16 +0000

    remove express server for vercel deployment

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
 const supabaseUrl = "https://vikzesimclynhzhavdrl.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpa3plc2ltY2x5bmh6aGF2ZHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MTcyNDgsImV4cCI6MjA4NTM5MzI0OH0.k44Eibz2i7VD2rA-JGZu_v0s3t3ruKdxHr6qZx-U1MQ";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
