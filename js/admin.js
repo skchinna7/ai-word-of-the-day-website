52bd5661930515ce5a1f861936518cefad68ed37
Palgun Shashi Kumar <skchinna7@gmail.com>
2026-03-14 11:07:48 +0000

    admin.html and js has been change of code

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://vikzesimclynhzhavdrl.supabase.co";
+const SUPABASE_URL = "https://qzatavslhtkioivemwlk.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpa3plc2ltY2x5bmh6aGF2ZHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MTcyNDgsImV4cCI6MjA4NTM5MzI0OH0.k44Eibz2i7VD2rA-JGZu_v0s3t3ruKdxHr6qZx-U1MQ";
 
 const supabase = window.supabase.createClient(
